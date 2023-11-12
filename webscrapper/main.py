bc0ef7bcafede879a15242fdbfe786e6c7fb23f9
koreabeginner96 <zx028906@naver.com>
2023-11-12 16:46:51 +0900

    refactor : main file
    다른파일에서 당겨오는 순서
    #첫번째 폴더네임 . 파일네임 import function 네임

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -1,33 +1,6 @@
 from requests import get
 from bs4 import BeautifulSoup
-
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term= "python"
-
-response = get(f"{base_url}{search_term}")
-if response.status_code != 200:
-    print("Can't request website")
-else:
-    result = []
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_="jobs")
-    for job_section in jobs:
-        job_posts = job_section.find_all('li')
-        job_posts.pop(-1)
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
-            link = anchor['href']
-            company, kind, region = anchor.find_all('span', class_="company")
-            title= anchor.find('span', class_='title')
-            print(company.string, kind.string, region.string, title.string)
-            job_data ={
-                'link' : f"https://weworkremotely.com{link}",
-                'company' : company.string,
-                'region' : region.string,
-                'position': title.string
-            }
-            result.append(job_data)
-    for i in result:
-        print(i)
-        print('////////')
+from extractors.wwr import extract_wwr_jobs
+#첫번째 폴더네임 . 파일네임 import function 네임
+jobs =extract_wwr_jobs("python")
+print(jobs)
