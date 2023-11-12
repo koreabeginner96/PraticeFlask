cd84918bb5d9a765363f3809e3ca4eb14e0a78c8
koreabeginner96 <zx028906@naver.com>
2023-11-12 16:25:51 +0900

    chore : Savingresult

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -8,16 +8,25 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    result = []
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all('section', class_="jobs")
-    for jobs_section in jobs :
-        job_posts = jobs_section.find_all('li')
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
         job_posts.pop(-1)
         for post in job_posts:
             anchors = post.find_all('a')
-            anchor= anchors[1]
+            anchor = anchors[1]
             link = anchor['href']
-            company, kind, region = anchor.find_all('span', class_ ="company")
-            title = anchor.find('span', class_='tittle')
-            print(company,kind,region, title)
-            print('/////////')
+            company, kind, region = anchor.find_all('span', class_="company")
+            title= anchor.find('span', class_='title')
+            print(company.string, kind.string, region.string, title.string)
+            job_data ={
+                'company' : company.string,
+                'region' : region.string,
+                'position': title.string
+            }
+            result.append(job_data)
+    for i in result:
+        print(i)
+        print('////////')
