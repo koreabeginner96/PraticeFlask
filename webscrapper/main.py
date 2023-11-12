d18f11d3d71e7d225a9d86ec18ee6e15a15d18b7
koreabeginner96 <zx028906@naver.com>
2023-11-12 21:02:19 +0900

    chore : main.py add etc
    add : link,company,location,position

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -10,6 +10,7 @@ browser = webdriver.Chrome(options=options)
 
 browser.get("https://kr.indeed.com/jobs?q=python")
 
+result = []
 soup = BeautifulSoup(browser.page_source, "html.parser")
 job_list = soup.find("ul", class_="css-zu9cdh eu4oa1w0")
 jobs = job_list.find_all('li',recursive=False)
@@ -17,6 +18,18 @@ jobs = job_list.find_all('li',recursive=False)
 for job in jobs:
     zone = job.find("div", class_="mosaic-zone")
     if zone == None:
-        print("job li")
-    else:
-        print("mosaic li")
+        h2 = job.find("h2", class_= "jobTitle")
+        anchor = job.select_one("a")
+        title= anchor['aria-label']
+        link = anchor['href']
+        company = job.find("span", attrs={"data-testid":"company-name"})
+        location = job.find("div", attrs={"data-testid":"text-location"})
+        job_data={
+            'link' : f"https://kr.indeed.com{link}",
+            'company' : company.string,
+            'location' : location.string,
+            'position' : title
+        }
+        result.append(job_data)
+for r in result:
+    print(result,"\n///////\n")
