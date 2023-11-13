a59e475f44e0e6ae44710fbc2d6ee976b98fc05c
koreabeginner96 <zx028906@naver.com>
2023-11-13 14:17:16 +0900

    Feat : ADD Flask
    flsk start

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -1,13 +1,11 @@
-from extractors.indeed import extract_indeed_jobs
-from extractors.wwr import extract_wwr_jobs
-from file import save_to_file
+from flask import Flask
+#app = Flask("이름")에서 이름 부분에 공백(space)이 있으면 안된다.
+app = Flask("JobScrapper")
 
+#@app.route("/") = decorator / @가 있는 코드위에 함수를 위치 시켜야 한다.
+#func와 붙어 놓으면 flask는 주소(route 괄호 안)의 page 방문했을 때 아래 함수를 호출 시켜야하는 걸 안다.
+@app.route("/")
+def home():
+    return 'hey there!'
 
-keyword =input("what do u want to search for?")
-
-indeed = extract_indeed_jobs(keyword)
-wwr =extract_wwr_jobs(keyword)
-
-jobs = indeed + wwr
-
-print(save_to_file(keyword, jobs))
+app.run("0.0.0.0")
