0fd0cd8d39c657d2b75f7ce6f69cf41796f9f959
koreabeginner96 <zx028906@naver.com>
2023-11-13 16:31:26 +0900

    feat : add keyword func

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 #app = Flask("이름")에서 이름 부분에 공백(space)이 있으면 안된다.
 app = Flask("JobScrapper")
 
@@ -8,6 +8,7 @@ def home():
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    keyword=request.args.get("keyword")
+    return render_template("search.html",keyword=keyword)
 
 app.run("0.0.0.0")
