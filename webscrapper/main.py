7b5322fbfc156508e7f154b75cccb969110c03bb
koreabeginner96 <zx028906@naver.com>
2023-11-13 15:28:29 +0900

    feat: use templates
    템플릿을 활용하여 플라스크 브라우저 열기

--- webscrapper/main.py
+++ webscrapper/main.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template 
 #app = Flask("이름")에서 이름 부분에 공백(space)이 있으면 안된다.
 app = Flask("JobScrapper")
 
-#@app.route("/") = decorator / @가 있는 코드위에 함수를 위치 시켜야 한다.
-#func와 붙어 놓으면 flask는 주소(route 괄호 안)의 page 방문했을 때 아래 함수를 호출 시켜야하는 걸 안다.
 @app.route("/")
 def home():
-    return 'hey there!'
+    return render_template("home.html",name="nico")
 
 app.run("0.0.0.0")
